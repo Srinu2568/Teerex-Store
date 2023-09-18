eb7bd273beaa53ed7b733cac45a4c78a4d87e448
srinivas <mnvsrinivas1@gmail.com>
2023-09-18 12:52:12 +0530

    Initial Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<Routes>
+			<Route
+				path='/'
+				element={
+					<h1 className='m-4 text-2xl text-center p-3 px-2 font-bold underline'>
+						Hello world!
+					</h1>
+				}
+			></Route>
+		</Routes>
+	);
 }
 
-export default App;
